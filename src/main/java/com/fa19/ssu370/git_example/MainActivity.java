d6c59e09e7c5a54eaaa4896e93e8e4fa83f5f05b
Jeffrey Fisher <jeff@bam-043374-jf.local>
2019-11-11 17:53:39 -0800

    base src files

---
+++ src/main/java/com/fa19/ssu370/git_example/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fa19.ssu370.git_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
